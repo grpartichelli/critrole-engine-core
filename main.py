7a51b9a0fccabdcd07a72c613fcb6990a6a1bca1
grpartichelli <gabriel.partichelli@aegro.com.br>
2022-04-07 18:58:53 -0300

    fix: deploy fix

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from flask_cors import CORS
 
 from controllers.api_example import app_example
 
+app = Flask(__name__)
+
 
 def main():
-    app = Flask(__name__)
     app.register_blueprint(app_example)
     CORS(app)
     app.run()
