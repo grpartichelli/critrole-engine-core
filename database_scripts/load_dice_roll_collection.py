96bcfbf26e26ba39feb99adb9cfa4520acc12f03
grpartichelli <gabriel.partichelli@aegro.com.br>
2022-04-09 14:37:02 -0300

    feat: finish dice roll script

--- database_scripts/load_dice_roll_collection.py
+++ database_scripts/load_dice_roll_collection.py
@@ -5,29 +5,56 @@ import pandas as pd
 import math
 from datetime import datetime
 
+TOTAL_ROWS = 15365
+
 
 def run():
-    # dice_roll_repository.drop()
+    dice_roll_repository.drop()
     data = pd.read_excel(r'data/all-rolls.xlsx', sheet_name='All Episodes')
 
+    print("Start Loading Dice Rolls")
+    i = 0
+    last_percent = 0
     for index, row in data.iterrows():
         dice_roll = create_dice_roll_from_data_row(row)
+        dice_roll_repository.insert_one(dice_roll)
 
-        # dice_roll_repository.insert_one(dice_roll)
+        i += 1
+        percent = 100 * i / TOTAL_ROWS
+        if percent > last_percent:
+            print("LOADING: " + str(math.ceil(percent)) + "%")
+            last_percent = math.ceil(percent)
+
+    print("Finish Loading Dice Rolls")
 
 
 def create_dice_roll_from_data_row(row):
-    # row['Time']
-    dice_roll = DiceRoll(
+    return DiceRoll(
         convert_to_natural_value(row['Natural Value']),
-        row['Total Value'],
+        convert_to_total_value(row['Total Value']),
         convert_character_name(row['Character']),
         convert_to_was_enemy_killed(row['# Kills']),
-        row['Episode'],
+        convert_to_episode_number(row['Episode']),
         mongo_utils.timestamp_to_date(row['Time']),
         row['Type of Roll'].strip())
-    # dice_roll_repository.insert_one(dice_roll)
-    return dice_roll
+
+
+def convert_to_episode_number(value):
+    if isinstance(value, str):
+        value.strip()
+        value = value.removeprefix("C2E")
+        return int(value)
+    return None
+
+
+def convert_to_total_value(value):
+    if isinstance(value, float) or isinstance(value, int):
+        return value
+    if isinstance(value, str):
+        value.strip()
+        if value.startswith("Nat"):
+            return int(value.removeprefix("Nat"))
+    return None
 
 
 def convert_to_natural_value(value):
