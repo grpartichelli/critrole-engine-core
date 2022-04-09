c76cb681ed726f1ad49cabff7c4c4022923c1b7a
grpartichelli <gabriel.partichelli@aegro.com.br>
2022-04-09 13:44:06 -0300

    feat: add more conversions

--- database_scripts/load_dice_roll_collection.py
+++ database_scripts/load_dice_roll_collection.py
@@ -19,18 +19,41 @@ def run():
 def create_dice_roll_from_data_row(row):
     # row['Time']
     dice_roll = DiceRoll(
-        row['Natural Value'],
+        convert_to_natural_value(row['Natural Value']),
         row['Total Value'],
-        row['Character'],
+        convert_character_name(row['Character']),
         convert_to_was_enemy_killed(row['# Kills']),
         row['Episode'],
         mongo_utils.timestamp_to_date(row['Time']),
-        row['Type of Roll'])
+        row['Type of Roll'].strip())
     # dice_roll_repository.insert_one(dice_roll)
     return dice_roll
+
+
+def convert_to_natural_value(value):
+    if isinstance(value, float) or isinstance(value, int):
+        return value
+    return None
 
 
 def convert_to_was_enemy_killed(value):
     if isinstance(value, int) or isinstance(value, float):
         return not math.isnan(value)
     return False
+
+
+def convert_character_name(value):
+    value.strip()
+    if value == "Jester":
+        return "Jester Lavorre"
+    if value == "Molly":
+        return "Mollymauk Tealeaf"
+    if value == "Caduceus":
+        return "Caduceus Clay"
+    if value == "Yasha":
+        return "Yasha Nydoorin"
+    if value == "Caleb":
+        return "Caleb Widogast"
+    if value == "Beau":
+        return "Beauregard Lionett"
+    return value
