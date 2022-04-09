3280f350055aa3d33878b683eb7b3fcb126b6266
grpartichelli <gabriel.partichelli@aegro.com.br>
2022-04-09 17:21:47 -0300

    feat: create filtered_transcript_repository.py

---
+++ repositories/filtered_transcript_repository.py
@@ -0,0 +1,21 @@
+from models.transcript_model import Transcript
+from mongo import mongo_connector
+
+collection = mongo_connector.get_collection('FILTERED_TRANSCRIPTS')
+
+
+def drop():
+    collection.drop()
+
+
+def insert_one(transcript):
+    collection.insert_one(transcript.__dict__)
+
+
+def find_one(params):
+    return convert_mongo_to_transcript(collection.find_one(params))
+
+
+def convert_mongo_to_transcript(mongo_transcript):
+    del mongo_transcript['_id']
+    return Transcript(**mongo_transcript)
