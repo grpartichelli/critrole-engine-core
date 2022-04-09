57e06a04a42dca44c010c7cf608d095154410dee
grpartichelli <gabriel.partichelli@aegro.com.br>
2022-04-09 16:19:06 -0300

    feat: get actor name and episode number

--- database_scripts/load_transcript_collection.py
+++ database_scripts/load_transcript_collection.py
@@ -1,6 +1,7 @@
 from models.transcript_model import Transcript
 from repositories import transcript_repository
 from bs4 import BeautifulSoup
+from mongo import mongo_utils
 import os
 
 
@@ -12,6 +13,31 @@ def run():
     for file in os.listdir('data/transcripts/'):
         f = open(f'data/transcripts/{os.fsdecode(file)}', 'r')
         soup = BeautifulSoup(f.read(), 'html.parser')
+
+        episode_number = calculate_episode_number(soup)
+        actor_nickname = ''
+        for tag in soup.find_all(['dt', 'dd']):
+            if tag.name == "dt":
+                actor_nickname = tag.strong.string
+            if tag.name == "dt":
+                transcript = create_transcript_from_tag(tag, episode_number, actor_nickname)
+                print(mongo_utils.to_json(transcript))
         i += 1
-        if i == 3:
+        if i == 1:
             break
+
+
+def create_transcript_from_tag(tag, episode_number, actor_nickname):
+    transcript = Transcript(
+        actor_nickname,
+        None,
+        None,
+        None,
+        episode_number
+    )
+    return transcript
+
+
+def calculate_episode_number(soup):
+    data = soup.main.h3.string
+    return int(data.split().pop())
--- models/transcript_model.py
+++ models/transcript_model.py
@@ -1,6 +1,6 @@
 class Transcript:
-    def __init__(self, actor_name, timestamp, text, youtube_link, episode_number):
-        self.actor_name = actor_name
+    def __init__(self, actor_nickname, timestamp, text, youtube_link, episode_number):
+        self.actor_nickname = actor_nickname
         self.timestamp = timestamp
         self.text = text
         self.youtube_link = youtube_link
