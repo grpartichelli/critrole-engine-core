f2ccc160540cfc2a9924b922e1bcf457f0812cbe
grpartichelli <gabriel.partichelli@aegro.com.br>
2022-04-09 19:48:36 -0300

    feat: add security

--- load_database.py
+++ load_database.py
@@ -3,10 +3,14 @@ from database_scripts import load_character_collection, load_dice_roll_collectio
 
 
 def load_database():
-    load_character_collection.run()
-    load_combat_timestamp_collection.run()
-    load_transcript_collection.run()
-    load_dice_roll_collection.run()
+    text = input(
+        'Are you SURE you want to run this script?\n It will drop all tables and it may take hours to run.\n If you are sure type YES I\'M SURE\n')
+
+    if text == "YES I'M SURE":
+        load_character_collection.run()
+        load_combat_timestamp_collection.run()
+        load_transcript_collection.run()
+        load_dice_roll_collection.run()
 
 
 load_database()
