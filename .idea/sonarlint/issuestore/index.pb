ca17d84acf67330c4a98a4f7f423f3b2d02bf185
grpartichelli <gabriel.partichelli@aegro.com.br>
2022-04-25 17:01:28 -0300

    fix: character endpoint receiving list and number

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -6,8 +6,6 @@
 main.py,f/1/f1bdda93d9a278e358509d498e17d97764c1fb29
 H
 mongo/mongo_connector.py,9/e/9ed4259a52cdea8ff228f05a58573defca1a06ce
-T
-$repositories/character_repository.py,1/e/1ef6cf7d2a149f5d2ee83e6994901d335a9c7559
 L
 data/transcripts/cr2-40.html,b/2/b252c982046dfadc7e05d230d5240a8f057d0ee0
 L
@@ -24,8 +22,6 @@ P
  models/combat_timestamp_model.py,c/a/ca316c2fea2d810259fc02fe07d28da430d8c05c
 K
 data/transcripts/cr2-1.html,5/b/5be33d52c58f229b0ae42c86db3f6d164a8d5fb4
-D
-mongo/mongo_utils.py,6/0/60d06aebfa5a15d7d5fb4a509d3b8914a6677693
 J
 models/transcript_model.py,c/e/ce79b441b0d1252f415d781f51173b6260ebbc8e
 ^
@@ -36,5 +32,3 @@ P
 -database_scripts/load_dice_roll_collection.py,e/1/e17355afb7245ab16094b564b44527337e3ca2ee
 d
 4database_scripts/load_combat_timestamp_collection.py,3/8/38bb3ed90b8cda82ddd5a0d511269287838971b0
-]
--database_scripts/load_character_collection.py,8/8/886f691c9aa2f87b9d057da45ad9128bd85ea05f
--- controllers/character_controller.py
+++ controllers/character_controller.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request
 from flask_cors import cross_origin
 from mongo import mongo_utils
 from services import character_service
+import json
 
 character_controller = Blueprint('character_controller', __name__)
 
@@ -9,4 +10,9 @@ character_controller = Blueprint('character_controller', __name__)
 @character_controller.route('/api/character/')
 @cross_origin()
 def get_character():
-    return mongo_utils.to_json(character_service.get_character(request.args.get('name'), request.args.get('actor_name'), request.args.get('actor_nickname'), request.args.get('age'), request.args.get('pronouns'), request.args.get('creature_type'), request.args.get('race'), request.args.get('dnd_class')))
+    return mongo_utils.to_json(character_service.get_character(request.args.get('name'), request.args.get('actor_name'),
+                                                               request.args.get('actor_nickname'),
+                                                               request.args.get('age'),
+                                                               request.args.get('pronouns'),
+                                                               request.args.get('creature_type'),
+                                                               request.args.get('race'), request.args.get('dnd_class')))
